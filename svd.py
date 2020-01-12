1b5ac2db2c44d24714c188989222ccfbbbfdfd88
rlagksruf16 <rlagksruf16@gmail.com>
2020-01-13 01:12:20 +0900

    add code time to check svd time

--- svd.py
+++ svd.py
@@ -6,11 +6,16 @@ from numpy import array
 from numpy import dot
 import math
 from numpy.linalg import norm
+import timeit
 
 B = np.array([[4,1,1,4], [1,4,2,0] ,[2,1,4,5] ,[1,4,1,2]])
 
+#svd 실행
+start = timeit.default_timer()  #시작 시간
 U, s , V = np.linalg.svd(B, full_matrices=True)
+stop = timeit.default_timer()   #끝나는 시간
 
+print(stop-start)
 # print(U)
 # print(s)
 # print(V)
@@ -41,23 +46,4 @@ similar = cosine_similarity(appA,3)
 
 print(similar)
 
-
-# cos_sim = dot(appA[2],appA[3]/(norm(appA[2]) * norm(appA[3])))
-
-# print(cos_sim)
-# 코사인 유사도 검사 함수
-# def similar_cosine(data, name1, name2):
-#     sum_name1 = 0
-#     sum_name2 = 0
-#     sum_name1_name2 = 0
-#     for i in len(data):
-#         if i in len(data):
-#             sum_name1 += pow(data[name1][i], 2)
-#             sum_name2 += pow(data[name2][i], 2)
-#             sum_name1_name2 += data[name1][i]* data[name2][i]
-
-#     return sum_name1_name2 / (math.sqrt(sum_name1) * math.sqrt(sum_name2))
-
-# X = similar_cosine(appA,1,3)    
-# print(X)
 
